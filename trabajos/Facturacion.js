569496ee48f3b91c771ae7d31c54f0387eddebf3
charlycordoba <carlosarias_cba@hotmail.com>
2016-11-20 22:08:17 -0300

    Add files via upload

---
+++ trabajos/Facturacion.js
@@ -0,0 +1,88 @@
+
+/*1.	Para el departamento de facturación:
+A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
+B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
+C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+*/
+	function Sumar () 
+{
+	var precio1;
+var precio2;
+var precio3;
+var sumar;
+
+
+precio1= document.getElementById('PrecioUno').value;
+precio1=parseInt(precio1);
+
+precio2= document.getElementById('PrecioDos').value;
+precio2=parseInt(precio2);
+
+precio3=document.getElementById('PrecioTres').value;
+precio3=parseInt(precio3);
+
+sumar= precio1+precio2+precio3;
+
+alert("La suma es: "+ sumar);
+
+
+}
+function Promedio () 
+
+{
+	
+
+var precio1;
+	var precio2;
+	var precio3;
+	var promedio;
+	var suma;
+
+	precio1=document.getElementById("PrecioUno").value;
+	precio1=parseInt(precio1);
+
+	precio2=document.getElementById("PrecioDos").value;
+	precio2=parseInt(precio2);
+
+	precio3=document.getElementById("PrecioTres").value;
+	precio3=parseInt(precio3);
+
+suma=precio1+precio2+precio3;	
+	Promedio=suma/3;
+
+	alert("El promedio es: " + Promedio);
+
+
+	
+
+
+	
+}
+function PrecioFinal () 
+{
+	var precio1;
+	var precio2;
+	var precio3;
+	var promedio;
+	var suma;
+	var PrecioFinal;
+
+	precio1=document.getElementById("PrecioUno").value;
+	precio1=parseInt(precio1);
+
+	precio2=document.getElementById("PrecioDos").value;
+	precio2=parseInt(precio2);
+
+	precio3=document.getElementById("PrecioTres").value;
+	precio3=parseInt(precio3);
+
+suma=precio1+precio2+precio3;	
+	Promedio=suma/3;
+	PrecioFinal=suma*21/100
+
+	alert("El promedio es: " + PrecioFinal);
+
+
+	
+	
+}

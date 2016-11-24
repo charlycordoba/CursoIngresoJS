9dd9a5a22d60c9f35f60afba182fac592f6dd4fe
charlycordoba <carlosarias_cba@hotmail.com>
2016-11-24 18:13:31 -0300

    Add files via upload

---
+++ trabajos/function Mostrar()randomtp.js
@@ -0,0 +1,22 @@
+function Mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+
+	var RANDOM=5;
+
+
+	if (RANDOM>=9 && RANDOM<=10)
+	{
+		alert("Excelente");
+	}
+	if (RANDOM>4) 
+{ 
+	alert("Aprobo");
+
+}
+else
+ {
+	alert(" Vamos, la proxima se puede ");
+}
+
+}//FIN DE LA FUNCIÓN

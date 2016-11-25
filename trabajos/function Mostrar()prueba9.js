ec5eb73ea31c7cd78a4238fb6a0bb091f9a7b727
charlycordoba <carlosarias_cba@hotmail.com>
2016-11-25 12:03:40 -0300

    Add files via upload

---
+++ trabajos/function Mostrar()prueba9.js
@@ -0,0 +1,10 @@
+function Mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+	var miRandom;
+	/*miRandom=Math.floor((Math.random() * 4) + 5); entre 5 y 8*/
+	miRandom=Math.floor((Math.random() * 10) + 20);
+
+	alert(miRandom);
+
+}//FIN DE LA FUNCIÓN
---
+++ trabajos/jsInstruccionIF (10)corregido.js
@@ -0,0 +1,74 @@
+function Mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+	
+	/*var miNota
+	var RANDOM= 6; esta mal
+miNota=math.floor((math.RANDOM() *10) +1);
+
+	
+	if( RANDOM<=9 && RANDOM>=10) esta mal
+
+	        
+    if(miNota>8)
+
+            {
+		alert("excelente");
+
+			}
+
+		else
+		    {
+
+if (miNota<4){
+
+
+		//if (RANDOM>4) esta mal 
+
+				alert("Aprobo");
+						
+			 }
+
+					
+
+
+					//else( RANDOM<4)esta mal
+					else
+			 {
+						alert("Vamos, la proxima se puede");
+
+			 }
+
+			}*/
+
+var miNota;
+
+miNota=Math.floor((Math.ramdom()*11) + 1);
+
+if(miNota>8){
+
+	alert("excelente");
+
+            } 
+
+            else {
+                
+                if(miNota<4)
+                	alert("Aprobo");
+                 }
+
+                 else
+                 {
+
+                 	alert("Vamos, la proxima se puede");
+                 }
+
+
+
+
+
+
+
+
+
+}FIN DE LA FUNCIÓN

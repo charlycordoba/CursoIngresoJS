b89556d6ede68196595b577c0392f329cd8e11f7
charlycordoba <carlosarias_cba@hotmail.com>
2016-11-25 00:21:24 -0300

    Add files via upload

---
+++ trabajos/FuncionIf9.js
@@ -0,0 +1,37 @@
+function Mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+	
+	var RANDOM= +1;
+
+	
+	if( RANDOM){
+
+		alert("El numero es 1");
+
+					}
+					if (RANDOM) {
+
+
+						alert("el numero es 2");
+					}
+
+					if (RANDOM) 
+					{
+						alert("el numero es 3");
+					}
+
+
+					else( RANDOM=11)
+					{
+						alert("es mayor de 10, pasa...");
+
+					}
+
+
+
+
+
+
+
+}//FIN DE LA FUNCIÓN

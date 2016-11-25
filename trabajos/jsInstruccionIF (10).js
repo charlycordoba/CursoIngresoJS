dd698bc543bcdbad56161f8f61da26d453dc2409
charlycordoba <carlosarias_cba@hotmail.com>
2016-11-25 08:30:10 -0300

    Add files via upload

---
+++ trabajos/jsInstruccionIF (10).js
@@ -0,0 +1,37 @@
+function Mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+	
+	
+	var RANDOM= 6;
+
+	
+	if( RANDOM<=9 && RANDOM>=10){
+
+		alert("excelente");
+
+					}
+					if (RANDOM>4) {
+
+
+						alert("Aprobo");
+					}
+
+					
+
+
+					else( RANDOM<4)
+					{
+						alert("Vamos, la proxima se puede");
+
+					}
+
+
+
+
+
+
+
+
+
+}//FIN DE LA FUNCIÓN
